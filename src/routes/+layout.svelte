d16f354ed1e9e748fed7f6c11811a63e11f65123
Encodexed <robbiegollan@gmail.com>
2025-01-27 00:47:29 +1100

    Adds basic landing page, goes dark mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,6 @@
 	let { children } = $props();
 </script>
 
-{@render children()}
+<main class="h-screen w-screen">
+	{@render children()}
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,14 @@
-<h1>Rallypoint</h1>
-<p>Welcome to Rallypoint.</p>
-<button type="button" class="btn btn-accent btn-gradient">Take the tour</button>
+<div class="flex h-full w-full flex-col items-center justify-center gap-4">
+	<h1
+		class="w-fit bg-gradient-to-r from-blue-500 to-red-500 bg-clip-text text-6xl/relaxed font-black text-transparent"
+	>
+		Rallypoint
+	</h1>
+	<h3 class="-mt-2 text-xl font-semibold">
+		Complication-free coordination for faraway friends and family.
+	</h3>
+	<div class="mt-3 flex gap-6">
+		<button type="button" class="btn btn-accent btn-gradient w-40">Create an event</button>
+		<button type="button" class="btn btn-accent btn-gradient w-40">Join an event</button>
+	</div>
+</div>
